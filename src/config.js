2e883f90a9a092e2c44b19e4eb68676b31a474b4
d.sajedi <d.sajedi@fanap.ir>
2024-05-06 16:14:05 +0330

    server fixed.

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const HOST_URL = "127.0.0.1";
-const USER = "dvAdmin";
-const PASS = "JTaw_99@+,JS";
-const DATABASE = "dvdevir1_todolist";
+const USER = "root";
+const PASS = "";
+const DATABASE = "todolist";
 
 module.exports = {HOST_URL, USER, PASS, DATABASE}
